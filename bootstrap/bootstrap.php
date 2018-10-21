c700b0cd8ef9426bf963b7bb0d3eca3082ef87d3
Nima Ebrazi <nima.ebrazi1373@gmail.com>
2018-10-21 10:20:10 +0330

    change env construct

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -2,8 +2,7 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = new Dotenv\Dotenv(__DIR__ . '/..');
-$dotenv->load();
+$dotenv = (new Dotenv\Dotenv(__DIR__ . '/..'))->load();
 
 $setting = require __DIR__ . '/../bootstrap/settings.php';
 
