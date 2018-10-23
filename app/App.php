3151dc07cf689f4018c7cf994e6eabea679e17a5
Nima Ebrazi <nima.ebrazi1373@gmail.com>
2018-10-23 22:47:09 +0330

    Refactore bootstrap file

--- app/App.php
+++ app/App.php
@@ -3,14 +3,35 @@
 
 namespace App;
 
-use \DI\Bridge\Slim\App as Application;
+use DI\Bridge\Slim\App as Application;
 use DI\ContainerBuilder;
+use Illuminate\Database\Capsule\Manager;
+use Illuminate\Database\Capsule\Manager as Capsule;
 
 class App extends Application
 {
+    /**
+     * Add container config to container builder.
+     *
+     * @param ContainerBuilder $builder
+     */
     protected function configureContainer(ContainerBuilder $builder)
     {
         $builder->addDefinitions(__DIR__ . './../bootstrap/container.php');
     }
 
+    /**
+     * Boot database with global access in whole of project.
+     *
+     * @param array $config
+     * @return Capsule
+     */
+    public function bootDataBase(array $config): Manager
+    {
+        $capsule = new Capsule();
+        $capsule->addConnection($config);
+        $capsule->setAsGlobal();
+        $capsule->bootEloquent();
+        return $capsule;
+    }
 }
--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -1,23 +1,39 @@
 <?php
 
 use App\App;
-use Noodlehaus\Config;
 use Dotenv\Dotenv;
+use Noodlehaus\Config;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+/*
+|--------------------------------------------------------------------------
+| Load env File
+|--------------------------------------------------------------------------
+|
+| Config .env file don't track with CVS like git and created from
+| .env.example template. Dotenv load it from root directory of project.
+|
+*/
 $dotenv = (new Dotenv(__DIR__ . '/..'))->load();
-$config = new Config(__DIR__ . '/../config');
 
+/*
+|--------------------------------------------------------------------------
+| Load Config Files
+|--------------------------------------------------------------------------
+|
+| All application configs placed in {ROOT_DIR}/config/.
+| and application need load before create instance form it.
+| We load all file in config directory by bootstrap project.
+|
+*/
+$config = new Config(__DIR__ . '/../config');
 
 $app = new App();
 
-$capsule = new Illuminate\Database\Capsule\Manager();
-$capsule->addConnection(
+$capsule = $app->bootDataBase(
     $config->get('database')
 );
-$capsule->setAsGlobal();
-$capsule->bootEloquent();
 
 
 // Register middleware
